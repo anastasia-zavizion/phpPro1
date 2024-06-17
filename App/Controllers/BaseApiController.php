3a505d476f5c6ca1d16990dc8003bcea62f8b5d9
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-06-17 22:48:14 +0300

    BaseApiController fix

--- App/Controllers/BaseApiController.php
+++ App/Controllers/BaseApiController.php
@@ -1,8 +1,16 @@
 <?php
-namespace Controllers;
+namespace App\Controllers;
+use App\Controllers\Controller;
+use App\Models\User;
+use ReallySimpleJWT\Token;
 
 class BaseApiController extends Controller{
     public function before(string $action,array $params):bool{
+        $token = getAuthToken();
+        $user = User::find(authId());
+        if(!Token::validate($token, $user->password)){
+            throw new Exception('Token is invalid', 422);
+        }
         return true;
     }
 }
